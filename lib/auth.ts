fc19794e201b5418af11ffcf205018ee06c92bea
Hayeon(Alexia) Kim <alexiak@bu.edu>
2025-12-06 18:24:43 -0500

    google auth implemented

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,8 @@
 "use server";
 import { signIn, signOut} from "@/auth";
 
-export const logIn = async() => {
-    await signIn("github");
+export const logIn = async(provider: "github" | "google") => {
+    await signIn(provider);
 }
 
 export const logOut = async() => {
